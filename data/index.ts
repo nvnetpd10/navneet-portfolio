670d8dea2d31b6a7665cd610d8a0ecf3578ebb9a
nvnetpd10 <navneettiwarinew270103@gmail.com>
2024-06-23 23:19:52 +0530

    all sort

--- data/index.ts
+++ data/index.ts
@@ -220,13 +220,16 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
+      url: "https://github.com/nvnetpd10",
     },
     {
       id: 2,
       img: "/twit.svg",
+      url: "https://x.com/NavneetTiwari27",
     },
     {
       id: 3,
       img: "/link.svg",
+      url: "https://www.linkedin.com/in/navneet-tiwari-9209a3218/",
     },
   ];
